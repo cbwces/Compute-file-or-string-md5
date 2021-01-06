62a6c38db243b0fd4117b181954c4ac9a8d3192c
cbwces <870974964@qq.com>
2021-01-07 02:58:42 +0800

    modify3

--- main_md5.cpp
+++ main_md5.cpp
@@ -5,34 +5,34 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
-#include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 //#define READ_DATA_SIZE	500000
 //#define MD5_SIZE		16
 //#define MD5_STR_LEN		32
 
-extern const unsigned long READ_DATA_SIZE = 1000000;
+//extern const unsigned long READ_DATA_SIZE = 1000000;
+extern const unsigned long READ_DATA_SIZE = 8192;
 extern const unsigned short MD5_SIZE = 16;
 extern const unsigned short MD5_STR_LEN = 32;
 
 // function declare
 std::string Compute_file_md5(const char *file_path, char *md5_str);
-void Check_in_map(const std::string&, const std::string& file_path, std::map<std::string, std::string>& md5_container, std::set<std::string>& repeat_set);
+void Check_in_map(const std::string&, const std::string& file_path, std::unordered_map<std::string, std::string>& md5_container, std::unordered_set<std::string>& repeat_set);
 
 /************** main test **************/
 int main(int argc, char *argv[])
 {
-    std::map<std::string, std::string> md5_container;
-    std::set<std::string> repeat_set;
+    std::unordered_map<std::string, std::string> md5_container;
+    std::unordered_set<std::string> repeat_set;
 	char md5_str[MD5_STR_LEN + 1];
     std::string true_md5_str;
     std::string file_path;
     std::ifstream img_reader;
-    img_reader.open(argv[1], std::ios::in);
+    img_reader.open(argv[1]);
     while (img_reader >> file_path){
         true_md5_str = Compute_file_md5(file_path.c_str(), md5_str);
         Check_in_map(true_md5_str, file_path, md5_container, repeat_set);
@@ -44,15 +44,13 @@ int main(int argc, char *argv[])
     }
     img_reader.close();
     for (auto p=repeat_set.begin(); p != repeat_set.end(); ++p){
-        printf((*p).c_str());
-        printf("\n");
-        //std::cout << *p + "\n"; 
+        printf("%s \n", (*p).c_str());
     }
 	return 0;
 }
 
-void Check_in_map(const std::string& true_md5_str, const std::string& file_path, std::map<std::string, std::string>& md5_container, std::set<std::string>& repeat_set){
-    std::map<std::string, std::string>::iterator pt = md5_container.find(true_md5_str);
+void Check_in_map(const std::string& true_md5_str, const std::string& file_path, std::unordered_map<std::string, std::string>& md5_container, std::unordered_set<std::string>& repeat_set){
+    std::unordered_map<std::string, std::string>::iterator pt = md5_container.find(true_md5_str);
     if (pt != md5_container.end()){
         repeat_set.insert((*pt).second);
         repeat_set.insert(file_path);
@@ -101,8 +99,6 @@ std::string Compute_file_md5(const char *file_path, char *md5_str)
             break;
         }
     }
-    //ret = read(fd, data, READ_DATA_SIZE);
-    //MD5Update(&md5, data, ret);
 
 	close(fd);
 	MD5Final(&md5, md5_value);
@@ -110,7 +106,7 @@ std::string Compute_file_md5(const char *file_path, char *md5_str)
 	// convert md5 value to md5 string
 	for(int i = 0; i < MD5_SIZE; i++)
 	{
-		snprintf(md5_str + i*2, 2+1, "%02x", md5_value[i]);
+        snprintf(md5_str + i*2, 2+1, "%02x", md5_value[i]);
 	}
     return std::string(md5_str);
 }
